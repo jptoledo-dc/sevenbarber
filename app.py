648f3e2305503e5774aaf3e0f6c5b9620a3c6001
João Paulo Toledo <80119199+jptoledo-dc@users.noreply.github.com>
2024-07-16 19:55:00 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
-import webbrowser
+import pandas as pd
 from PIL import Image
+import requests
+import datetime
+import time
 
 icon = Image.open('seven.png')
 st.set_page_config(
@@ -8,6 +11,27 @@ st.set_page_config(
     page_icon= icon,
     layout="wide"
 )
+log = pd.read_csv("log.csv")
+
+
+response = requests.get("https://ipinfo.io/json")
+ip_info = response.json()
+data = {
+    "Timestamp": [datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
+    "IP": [ip_info.get("ip")],
+    "City": [ip_info.get("city")],
+    "Region": [ip_info.get("region")],
+    "Country": [ip_info.get("country")],
+    "Location": [ip_info.get("loc")],
+    "Org": [ip_info.get("org")],
+    "Postal": [ip_info.get("postal")],
+    "Timezone": [ip_info.get("timezone")],
+    "Click": "First Login"
+}
+df = pd.DataFrame(data)
+df3= pd.concat([log, df], ignore_index=True)
+df3.to_csv("log.csv",index=False)
+
 # CSS para dimensionar e estilizar os botões
 button_style = """
 <style>
@@ -44,8 +68,49 @@ if erick.button("Agendar com Erick"):
     js = f"window.open('{url}');"
     html = f"<script>{js}</script>"
     st.components.v1.html(html)
+    st.success("Se você não foi redirecionado, por favor, habilite os pop-ups ou clique nesse link: https://abrir.link/zrfIn")
+    response = requests.get("https://ipinfo.io/json")
+    ip_info = response.json()
+    data = {
+        "Timestamp": [datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
+        "IP": [ip_info.get("ip")],
+        "City": [ip_info.get("city")],
+        "Region": [ip_info.get("region")],
+        "Country": [ip_info.get("country")],
+        "Location": [ip_info.get("loc")],
+        "Org": [ip_info.get("org")],
+        "Postal": [ip_info.get("postal")],
+        "Timezone": [ip_info.get("timezone")],
+        "Click": "Erick"
+    }
+    df = pd.DataFrame(data)
+    df3= pd.concat([log, df], ignore_index=True)
+    df3.to_csv("log.csv",index=False)
 
 matheus.image("matheus.png",caption = "Matheus Henrique - Contato: (12) 99770-3132")
 
 if matheus.button("Agendar com Matheus"):
     url = "https://wa.me/5512997703132?text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio"
+    js = f"window.open('{url}');"
+    html = f"<script>{js}</script>"
+    st.components.v1.html(html)
+    st.success("Se você não foi redirecionado, por favor, habilite os pop-ups ou clique nesse link: https://abrir.link/iMACX")
+    response = requests.get("https://ipinfo.io/json")
+    ip_info = response.json()
+    data = {
+        "Timestamp": [datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
+        "IP": [ip_info.get("ip")],
+        "City": [ip_info.get("city")],
+        "Region": [ip_info.get("region")],
+        "Country": [ip_info.get("country")],
+        "Location": [ip_info.get("loc")],
+        "Org": [ip_info.get("org")],
+        "Postal": [ip_info.get("postal")],
+        "Timezone": [ip_info.get("timezone")],
+        "Click": "Matheus"
+    }
+    df = pd.DataFrame(data)
+    df3= pd.concat([log, df], ignore_index=True)
+    df3.to_csv("log.csv",index=False)
+
+
